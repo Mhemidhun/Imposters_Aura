c24ecb458572f289903cd96dbf421c8f61d53fa3
YaseerM16 <yaseerbscit@gmail.com>
2025-03-15 16:20:03 +0530

    done product management necessities

--- backend/src/repositories/adminRepository/productRepository.ts
+++ backend/src/repositories/adminRepository/productRepository.ts
@@ -12,6 +12,14 @@ export default class ProductRepository extends BaseRepository<{
             Product: ProductModel
         })
     }
+    async getProducts(): Promise<IProducts[]> {
+        try {
+            const products = await this.findAll("Product", { isBlocked: false })
+            return products
+        } catch (error) {
+            throw error
+        }
+    }
 
     async addProduct(data: productInput): Promise<IProducts> {
         try {
@@ -30,4 +38,72 @@ export default class ProductRepository extends BaseRepository<{
             throw error
         }
     }
+
+    async editProduct(productId: string, data: Partial<productInput>): Promise<IProducts> {
+        try {
+            const existingProduct = await this.findById("Product", productId);
+            if (!existingProduct) {
+                throw new Error("Product not found");
+            }
+            const updatedProduct = await this.updateById(
+                "Product",
+                productId,
+                data
+            );
+
+            if (!updatedProduct) {
+                throw new Error("Failed to update product");
+            }
+
+            return updatedProduct;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async blockProduct(productId: string): Promise<IProducts> {
+        try {
+            const existingProduct = await this.findById("Product", productId);
+            if (!existingProduct) {
+                throw new Error("Product not found");
+            }
+            const updatedProduct = await this.updateById(
+                "Product",
+                productId,
+                { isBlocked: true }
+            );
+
+            if (!updatedProduct) {
+                throw new Error("Failed to update product");
+            }
+
+            return updatedProduct;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async unBlockProduct(productId: string): Promise<IProducts> {
+        try {
+            const existingProduct = await this.findById("Product", productId);
+            if (!existingProduct) {
+                throw new Error("Product not found");
+            }
+            const updatedProduct = await this.updateById(
+                "Product",
+                productId,
+                { isBlocked: false }
+            );
+
+            if (!updatedProduct) {
+                throw new Error("Failed to update product");
+            }
+
+            return updatedProduct;
+        } catch (error) {
+            throw error
+        }
+    }
+
 }
